59a43f678ee0a9b9db3c6ac3ade445872342e08c
Bhavika Varshney <bhavika.varshney@gmail.com>
2024-09-02 16:26:59 +0530

    Components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () =>{
+    return(
+        <section 
+            id = "Footer"
+            className="flex-col text-black w-[100%] flex items-center px-[5%] md:px-[10%]">
+                    <div className="text-black">
+                        {/* Footer */}
+                    </div>
+                
+        </section>
+
+    );
+};
+
+export default About;
